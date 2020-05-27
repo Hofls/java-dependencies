e3dce639654f3a033ee59497a23cd8b480dc2a80
Hofls <pleshakov@proitr.ru>
2020-05-27 11:51:42 +0500

    Date-time example

---
+++ java-spring/src/main/java/hofls/com/github/datetime/DateTimeDemo.java
@@ -0,0 +1,11 @@
+package hofls.com.github.datetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DateTimeDemo {
+
+    LocalDate saneDate = LocalDate.now();
+    LocalDateTime saneDateTime = LocalDateTime.now();
+
+}
