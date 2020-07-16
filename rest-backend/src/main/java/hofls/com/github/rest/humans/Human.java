84b61370852c125dcd0fc25f2d6285867bc60f44
Hofls <pleshakov@proitr.ru>
2020-07-16 20:44:37 +0500

    Springfox List example

--- rest-backend/src/main/java/hofls/com/github/rest/humans/Human.java
+++ rest-backend/src/main/java/hofls/com/github/rest/humans/Human.java
@@ -3,10 +3,14 @@ package hofls.com.github.rest.humans;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Human {
     @ApiModelProperty(value = "Full name of a person", example = "John St Junior")
     private String name;
     @ApiModelProperty(value = "Main profession", example = "Blacksmith")
     private String profession;
+    @ApiModelProperty(value = "Objects of profound interest", example = "[\"Football\",\"Music\"]")
+    private List<String> hobbies;
 }
