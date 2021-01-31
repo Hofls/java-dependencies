95617455cfc59aa3fc3d0383cccf98d68d2b7863
Hofls <hofls@ya.ru>
2021-01-31 17:15:58 +0500

    Tests with auto-generated IDs

--- h2db/src/test/java/hofls/com/github/javahibernateexample/storage/jpa_repository/CampusServiceTest.java
+++ h2db/src/test/java/hofls/com/github/javahibernateexample/storage/jpa_repository/CampusServiceTest.java
@@ -1,10 +1,14 @@
 package hofls.com.github.javahibernateexample.storage.jpa_repository;
 
+import hofls.com.github.javahibernateexample.storage.junit.TestUtils;
 import lombok.val;
+import lombok.var;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +17,9 @@ public class CampusServiceTest extends BaseTest {
     @Autowired
     private CampusService service;
 
+    @Autowired
+    private CampusRepository repository;
+
     @Test
     // Try to remove "UTF-8", run `mvn test`, it will break on Windows (default encoding Windows-1252)
     @Sql(config = @SqlConfig(encoding = "UTF-8"), scripts = "findById_test.sql")
@@ -20,5 +27,16 @@ public class CampusServiceTest extends BaseTest {
         val campus = service.findById(23L);
         assertEquals("Cyrillic campus: абвгд", campus.getName());
     }
+
+    @Test
+    public void save_test() throws IOException {
+        val campus = new Campus();
+        campus.setName("English campus");
+        val actual = repository.save(campus);
+        var expected = TestUtils.readFile(this.getClass(), "save_test.json");
+        expected = expected.replace("\"INSERT_ID_HERE\"", String.valueOf(actual.getId()));
+        TestUtils.assertEqualJson(expected, actual);
+    }
+
 
 }
