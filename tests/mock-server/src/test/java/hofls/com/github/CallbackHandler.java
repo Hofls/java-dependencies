6083c55f7daf674b5d1deb41158b9a83c50a71c1
Hofls <hofls@ya.ru>
2021-06-28 21:23:14 +0500

    Mock-server

--- tests/mock-server/src/test/java/hofls/com/github/CallbackHandler.java
+++ tests/mock-server/src/test/java/hofls/com/github/CallbackHandler.java
@@ -4,13 +4,9 @@ import org.mockserver.mock.action.ExpectationCallback;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 
-import static org.mockserver.model.HttpResponse.notFoundResponse;
 import static org.mockserver.model.HttpResponse.response;
 
 public class CallbackHandler implements ExpectationCallback {
-
-    public static HttpResponse httpResponse = response()
-            .withStatusCode(200);
 
     public HttpResponse handle(HttpRequest httpRequest) {
         String body = httpRequest.getBodyAsString();
