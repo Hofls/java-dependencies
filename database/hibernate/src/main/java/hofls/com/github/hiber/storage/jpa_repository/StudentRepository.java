a97c0d3913c4acd7f000eaf4dfd685343321b43c
Hofls <hofls@ya.ru>
2021-07-07 12:15:47 +0500

    hibernate @Modifying @Query

--- database/hibernate/src/main/java/hofls/com/github/hiber/storage/jpa_repository/StudentRepository.java
+++ database/hibernate/src/main/java/hofls/com/github/hiber/storage/jpa_repository/StudentRepository.java
@@ -4,6 +4,7 @@ import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -23,5 +24,10 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findBadBoys();
     @Query("select s from Student s where s.name = :personName and s.campus.name = :campusName")
     List<Student> findBadBoys(String personName, String campusName);
+
+    /** Rare case when you need to update only specific field, to prevent overriding other fields (parallel editing) */
+    @Modifying
+    @Query("UPDATE Student s SET s.name = ?1 WHERE s.id = ?2")
+    void updateName(String name, Long id);
 
 }
