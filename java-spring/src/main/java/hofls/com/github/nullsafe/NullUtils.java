f31e41e2c3f6fc02c8f1ff30ec3bb0ec7520ff1e
Hofls <hofls@ya.ru>
2021-09-14 16:24:36 +0500

    Moved to NullUtils

--- java-spring/src/main/java/hofls/com/github/nullsafe/NullUtils.java
+++ java-spring/src/main/java/hofls/com/github/nullsafe/NullUtils.java
@@ -1,6 +1,11 @@
 package hofls.com.github.nullsafe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class NullUtils {
 
@@ -18,6 +23,19 @@ public class NullUtils {
         } catch (NullPointerException | IndexOutOfBoundsException e) {
             return defaultValue;
         }
+    }
+
+    /** To avoid NullPointerException in cycles */
+    public static <T> List<T> safe(List<T> list) {
+        return list == null ? Collections.emptyList() : list;
+    }
+
+    public static <T> List<T> removeNulls(List<T> elements) {
+        if (org.springframework.util.CollectionUtils.isEmpty(elements)) {
+            return new ArrayList<>();
+        }
+
+        return elements.stream().filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 }
