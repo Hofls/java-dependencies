4d3078d3bb8df152ca9d9493ef0df8a4e0bbc543
Hofls <hofls@ya.ru>
2021-12-28 20:09:09 +0500

    kotlin-vs-groovy

--- gradle/kotlin-vs-groovy/build-syntax.gradle.kts
+++ gradle/kotlin-vs-groovy/build-syntax.gradle.kts
@@ -18,8 +18,14 @@ application {
     mainClass.set("org.sample.myapp.Main")
 }
 
+val swaggerVersion = "2.9.2"
+val lombokVersion = "1.18.20"
+
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:2.4.5"))
+
+    implementation("io.springfox:springfox-swagger2:${swaggerVersion}")
+    implementation("io.springfox:springfox-swagger-ui:${swaggerVersion}")
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.sample:number-utils:1.0")
