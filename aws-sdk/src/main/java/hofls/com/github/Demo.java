1c95dc4fe84aa393650c5016367f47059ae015c8
Hofls <hofls@ya.ru>
2022-03-06 17:00:23 +0500

    aws sdk sns

--- aws-sdk/src/main/java/hofls/com/github/Demo.java
+++ aws-sdk/src/main/java/hofls/com/github/Demo.java
@@ -3,6 +3,8 @@ package hofls.com.github;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import lombok.var;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -17,6 +19,7 @@ public class Demo {
     @EventListener(ApplicationReadyEvent.class) // or @PostConstruct
     public void doSomethingAfterStartup() {
         S3();
+        SNS();
     }
 
     public static void S3() {
@@ -29,6 +32,21 @@ public class Demo {
         for (var bucket : s3.listBuckets()) {
             System.out.println("Bucket - " + bucket.getName());
         }
+        s3.putObject("bucket-from-sdk", "greeting", "hello world!");
+        var object = s3.getObjectAsString("bucket-from-sdk", "greeting");
+        System.out.println("Object from bucket - " + object);
+        s3.deleteObject("bucket-from-sdk", "greeting");
+        s3.deleteBucket("bucket-from-sdk");
+    }
+
+    public static void SNS() {
+        var configuration = new AwsClientBuilder.EndpointConfiguration(ENDPOINT, REGION);
+        final AmazonSNS sns = AmazonSNSClientBuilder.standard()
+                .withEndpointConfiguration(configuration)
+                .build();
+        sns.createTopic("topic-from-sdk");
+        var topic = sns.listTopics().getTopics().get(0).toString();
+        System.out.println("Topic - " + topic);
     }
 
 }
