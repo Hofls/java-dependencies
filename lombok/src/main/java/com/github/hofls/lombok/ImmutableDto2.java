36b9c9f0890bb29e94f1181802915a64b12e0cad
Hofls <hofls@ya.ru>
2022-06-13 19:31:55 +0500

    lombok @FieldNameConstants

--- lombok/src/main/java/com/github/hofls/lombok/ImmutableDto2.java
+++ lombok/src/main/java/com/github/hofls/lombok/ImmutableDto2.java
@@ -3,9 +3,11 @@ package com.github.hofls.lombok;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.FieldNameConstants;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Builder
+@FieldNameConstants // Access field names, e.g. ImmutableDto2.Fields.name (alternative to hibernate-jpamodelgen)
 public class ImmutableDto2 {
     Long id;
     String name;
