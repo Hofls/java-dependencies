f7ed21a82e8e7438f3175ba0fdb93e68d473cccc
Hofls <hofls@ya.ru>
2022-10-25 17:14:11 +0500

    Mapstruct - CommonMapperConfig

---
+++ mapstruct/src/main/java/hofls/com/github/mapstruct/common/CommonMapperConfig.java
@@ -0,0 +1,12 @@
+package hofls.com.github.mapstruct.common;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+// Usage - @Mapper(config = CommonMapperConfig.class)
+@MapperConfig(
+        nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+public class CommonMapperConfig {
+}
