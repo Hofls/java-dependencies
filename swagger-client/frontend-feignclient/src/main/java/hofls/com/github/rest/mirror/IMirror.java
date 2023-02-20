efa5c7938fb07391db896372a8736c87e575fb87
Hofls <hofls@ya.ru>
2023-02-20 15:03:42 +0500

    SpringQueryMap

--- swagger-client/frontend-feignclient/src/main/java/hofls/com/github/rest/mirror/IMirror.java
+++ swagger-client/frontend-feignclient/src/main/java/hofls/com/github/rest/mirror/IMirror.java
@@ -2,10 +2,10 @@ package hofls.com.github.rest.mirror;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /** In real project this interface would be imported via gradle/maven */
 @Tag(name = "Mirror", description = "Reflects everything back")
@@ -15,6 +15,6 @@ public interface IMirror {
     // @SpringQueryMap - allows to use POJO as GET parameter (for @FeignClient)
     @Operation(summary = "Reflects value back")
     @GetMapping(value="/{value}")
-    String reflect(@PathVariable String value);
+    String reflect(@SpringQueryMap @PathVariable String value);
 
 }
