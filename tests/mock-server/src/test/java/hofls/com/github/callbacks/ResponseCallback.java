d7de57d083df2f48f6dcdd43b2e0c37b847bfa6b
Hofls <hofls@ya.ru>
2023-03-10 13:28:11 +0500

    mock server

--- tests/mock-server/src/test/java/hofls/com/github/callbacks/ResponseCallback.java
+++ tests/mock-server/src/test/java/hofls/com/github/callbacks/ResponseCallback.java
@@ -9,6 +9,7 @@ import static org.mockserver.model.HttpResponse.response;
 public class ResponseCallback implements ExpectationCallback {
 
     public HttpResponse handle(HttpRequest httpRequest) {
+        // Sometimes also needed .withHeaders(new Header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString()))
         return response()
                 .withStatusCode(200).withBody("{\"numbers\":[1, 2, 3]}");
     }
