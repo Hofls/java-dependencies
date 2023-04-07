2d0b4f16acd2e2de10cc4875d4be4440d7eb14e8
Hofls <hofls@ya.ru>
2023-04-07 22:38:25 +0500

    http patch - refactoring

--- swagger-backend/springdoc/src/main/java/hofls/com/github/rest/patch/common/PatchListService.java
+++ swagger-backend/springdoc/src/main/java/hofls/com/github/rest/patch/common/PatchListService.java
@@ -9,15 +9,15 @@ import java.util.UUID;
 @Service
 public class PatchListService <T extends Identifiable, K extends IPatch> {
 
-    public void applyPatch(List<T> units, List<K> patchUnits, IPatchService patchableEntity) {
+    public void applyPatch(List<T> units, List<K> patchUnits, IPatchService patchService) {
         if (CollectionUtils.isEmpty(patchUnits)) {
             return;
         }
 
         for (IPatch patchUnit : patchUnits) {
             if (patchUnit.getOperation() == PatchOperation.ADD) {
-                Identifiable unit = patchableEntity.newEntity();
-                patchableEntity.toEntity(unit, patchUnit);
+                Identifiable unit = patchService.newEntity();
+                patchService.toEntity(unit, patchUnit);
                 if (patchUnit.getId() == null) {
                     unit.setId(UUID.randomUUID());
                 }
@@ -30,7 +30,7 @@ public class PatchListService <T extends Identifiable, K extends IPatch> {
                 if (unit == null) {
                     throw new RuntimeException("Entity not found. Id - " + patchUnit.getId());
                 }
-                patchableEntity.toEntity(unit, patchUnit);
+                patchService.toEntity(unit, patchUnit);
             } else if (patchUnit.getOperation() == PatchOperation.REMOVE) {
                 UUID patchUnitId = UUID.fromString(patchUnit.getId());
                 units.removeIf(u -> u.getId().equals(patchUnitId));
