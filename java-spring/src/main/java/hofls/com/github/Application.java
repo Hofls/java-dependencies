d2d3f719eddeba2df61d485728b48d7519576b3d
Hofls <hofls@ya.ru>
2023-05-12 11:25:16 +0500

    spring cache clear

--- java-spring/src/main/java/hofls/com/github/Application.java
+++ java-spring/src/main/java/hofls/com/github/Application.java
@@ -3,9 +3,11 @@ package hofls.com.github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Locale;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
--- java-spring/src/main/java/hofls/com/github/cache/CacheConf.java
+++ java-spring/src/main/java/hofls/com/github/cache/CacheConf.java
@@ -1,16 +1,25 @@
 package hofls.com.github.cache;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableCaching
 @Configuration
 public class CacheConf{
+
     @Bean(name = "cacheableRepositoryManager")
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("repository-cache");
+    }
+
+    @CacheEvict(allEntries = true, value = "repository-cache")
+    @Scheduled(fixedDelay = 30 * 60 * 1000) // Won't work without @EnableScheduling
+    public void clearCache() {
+        System.out.println("CACHE CLEARED");
     }
 }
