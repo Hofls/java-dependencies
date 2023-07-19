0c35d4a45d0a017c4a680d8ca9d8cae261bfe459
Hofls <hofls@ya.ru>
2023-07-19 12:22:28 +0500

    csv reader

--- file/csv/src/main/java/hofls/com/github/OnStartup.java
+++ file/csv/src/main/java/hofls/com/github/OnStartup.java
@@ -4,13 +4,22 @@ import hofls.com.github.service.CsvReader;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Service
 public class OnStartup {
 
     @PostConstruct
     public void init() {
-        new CsvReader().readCsv();
+        List<String> linesA = new CsvReader().readCsv(
+                "/example.csv",
+                cells -> cells[0] + ";" + cells[1]);
+        System.out.println(linesA);
+
+        List<String> linesB = new CsvReader().readCsv(
+                "/math.csv",
+                cells -> cells[0] + "+" + cells[1] + "=" + cells[2]);
+        System.out.println(linesB);
     }
 
 }
--- file/csv/src/main/java/hofls/com/github/service/CsvReader.java
+++ file/csv/src/main/java/hofls/com/github/service/CsvReader.java
@@ -3,30 +3,30 @@ package hofls.com.github.service;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 public class CsvReader {
 
-    public void readCsv() {
-        try (InputStream inputStream = CsvReader.class.getResourceAsStream("/example.csv");
+    public <T> List<T> readCsv(String file, Function<String[], T> mapper) {
+        List<T> result = new ArrayList<>();
+        try (InputStream inputStream = CsvReader.class.getResourceAsStream(file);
              BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line = br.readLine(); // skip header
 
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(";");
-                Arrays.stream(data)
-                        .forEach(value -> System.out.print(value + " "));
-
-                for (int i = 0; i < data.length; i++) {
-                    String value = data[i];
-                    System.out.print(value + " ");
-                }
-                System.out.println(); // Print a new line after each row
+                T value = mapper.apply(data);
+                result.add(value);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return result;
     }
+
 
 }
