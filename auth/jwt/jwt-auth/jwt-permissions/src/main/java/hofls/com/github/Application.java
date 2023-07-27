64df5f92fe9a20a73d064d3abc72bbb9a9e98a60
Hofls <hofls@ya.ru>
2023-07-27 18:51:58 +0500

    jwt-permissions

--- auth/jwt/jwt-auth/jwt-permissions/src/main/java/hofls/com/github/Application.java
+++ auth/jwt/jwt-auth/jwt-permissions/src/main/java/hofls/com/github/Application.java
@@ -3,8 +3,10 @@ package hofls.com.github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 @EnableWebSecurity
 public class Application extends SpringBootServletInitializer {
