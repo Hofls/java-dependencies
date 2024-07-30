c024cd8553df91c88b4e8c49ca4f71e1d8eb66ff
Hofls <hofls@ya.ru>
2024-07-30 15:39:20 +0500

    lockById

--- database/hibernate/src/main/java/hofls/com/github/hiber/storage/locks_demo/pessimistic_lock/NotificationPessRepository.java
+++ database/hibernate/src/main/java/hofls/com/github/hiber/storage/locks_demo/pessimistic_lock/NotificationPessRepository.java
@@ -2,11 +2,21 @@ package hofls.com.github.hiber.storage.locks_demo.pessimistic_lock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
+import java.util.UUID;
 
 
 public interface NotificationPessRepository extends JpaRepository<NotificationPess, Long> {
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     NotificationPess findById(long id);
+
+    @Query(nativeQuery = true, value =
+        "SELECT np.* FROM notification_pess np" +
+        "WHERE np.id = :id FOR UPDATE")
+    NotificationPess lockById(@Param(value = "id") UUID id);
+
 }
