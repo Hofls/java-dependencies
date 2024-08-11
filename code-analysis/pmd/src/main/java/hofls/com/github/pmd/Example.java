cb73049fb9116805e7989d7a48ecac08cfa2ffaa
Hofls <hofls@ya.ru>
2024-08-11 15:06:16 +0500

    pmd

---
+++ code-analysis/pmd/src/main/java/hofls/com/github/pmd/Example.java
@@ -0,0 +1,11 @@
+package hofls.com.github.pmd;
+
+public class Example {
+
+    public void foo() {
+        int d = 23;
+        java.lang.String myString = "Hello, World!";
+        System.out.println(myString);
+    }
+
+}
