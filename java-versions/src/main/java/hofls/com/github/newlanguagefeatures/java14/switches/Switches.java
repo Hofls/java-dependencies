66aaa62470ce274d97cb5bbf6241ca4d263bf63a
Hofls <hofls@ya.ru>
2024-09-05 15:43:54 +0500

    java 14 - switch

--- java-versions/src/main/java/hofls/com/github/newlanguagefeatures/java14/switches/Switches.java
+++ java-versions/src/main/java/hofls/com/github/newlanguagefeatures/java14/switches/Switches.java
@@ -2,6 +2,10 @@ package hofls.com.github.newlanguagefeatures.java14.switches;
 
 public class Switches {
 
+    /**
+     * New and improved switch - no "break" necessary, works with strings, returns values
+     * Old switch uses ":", new switch uses "->"
+     */
     public int produceNumber(String text) {
         int result = switch (text) {
             case "a" -> 1;
