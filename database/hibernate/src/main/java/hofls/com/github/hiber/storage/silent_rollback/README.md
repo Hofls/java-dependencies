80a9d1e602a92ddab90aac6b9104ddecce0cdea0
Hofls <hofls@ya.ru>
2026-01-28 15:46:18 +0500

    silent rollback

--- database/hibernate/src/main/java/hofls/com/github/hiber/storage/silent_rollback/README.md
+++ database/hibernate/src/main/java/hofls/com/github/hiber/storage/silent_rollback/README.md
@@ -1,3 +1,6 @@
 ### Problem:
 * You caught exception, but still get error `UnexpectedRollbackException: Transaction silently rolled back because it has been marked as rollback-only`
 * Because `Spring` has proxies around `@Transactional` objects, he noticed exception and marked transaction for rollback
+* Fix - disable rollbacks for problematic method (look at `AddressService`)
+* Dead end - trying to force spring to print original exception, so debugging becomes easier
+  * No way to do it, OG exception is discarded
--- database/hibernate/src/test/java/hofls/com/github/hiber/storage/silent_rollback/SilentRollbackTest.java
+++ database/hibernate/src/test/java/hofls/com/github/hiber/storage/silent_rollback/SilentRollbackTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import hofls.com.github.hiber.storage.junit.BaseTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,7 +15,7 @@ public class SilentRollbackTest extends BaseTest {
 
     @Test
     public void problemStatement() {
-        Exception exception = Assertions.assertThrows(RuntimeException.class, () -> {
+        UnexpectedRollbackException exception = Assertions.assertThrows(UnexpectedRollbackException.class, () -> {
             userService.problemStatement();
         });
         assertEquals("Transaction silently rolled back because it has been marked as rollback-only", exception.getMessage());
