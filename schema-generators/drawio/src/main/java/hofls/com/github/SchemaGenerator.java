74f7e0677e5063bcc37ca3614c2451240cd08cfd
Hofls <hofls@ya.ru>
2026-02-17 17:11:41 +0500

    drawio

--- schema-generators/drawio/src/main/java/hofls/com/github/SchemaGenerator.java
+++ schema-generators/drawio/src/main/java/hofls/com/github/SchemaGenerator.java
@@ -1,7 +1,6 @@
 package hofls.com.github;
 
 import java.io.FileWriter;
-import java.lang.reflect.Field;
 import java.util.List;
 
 public class SchemaGenerator {
@@ -9,6 +8,10 @@ public class SchemaGenerator {
     record Table(String name, List<Field> fields) {}
 
     record Field(String cyrillicName, String englishName, String type) {}
+
+    private static final int ROW_HEIGHT = 14;
+    private static final int HEADER_HEIGHT = 30;
+    private static final int SPACING = 50;
 
     public void generate(List<Table> tables) {
         StringBuilder xml = new StringBuilder();
@@ -25,51 +28,59 @@ public class SchemaGenerator {
             Table table = tables.get(t);
             String tableId = "table_" + t;
 
-            // 1. Calculate Dynamic Width based on longest field string
-            int maxChars = table.name.length();
-            for (Field f : table.fields) {
-                String fullString = "+ " + f.cyrillicName + " (" + f.englishName + "): " + f.type;
-                if (fullString.length() > maxChars) maxChars = fullString.length();
-            }
+            // Calculate layout
+            int dynamicWidth = calculateWidth(table);
+            int totalHeight = HEADER_HEIGHT + (table.fields.size() * ROW_HEIGHT);
 
-            // Heuristic: ~7 pixels per char + padding. Minimum 180px.
-            int dynamicWidth = Math.max(180, (maxChars * 6) + 40);
-            int rowHeight = 14;
-            int headerHeight = 30;
-            int totalHeight = headerHeight + (table.fields.size() * rowHeight);
+            // Add Table Container
+            addTable(xml, table, tableId, xPosition, dynamicWidth, totalHeight);
 
-            // Table Style
-            String tableStyle = "shape=table;startSize=" + headerHeight + ";container=1;collapsible=0;childLayout=0;" +
-                    "fixedRows=1;rowLines=1;fontStyle=1;align=center;rounded=1;arcSize=12;whiteSpace=wrap;html=1;";
-
-            xml.append(String.format("<mxCell id=\"%s\" value=\"%s\" style=\"%s\" vertex=\"1\" parent=\"1\">",
-                    tableId, table.name, tableStyle));
-            xml.append(String.format("<mxGeometry x=\"%d\" y=\"80\" width=\"%d\" height=\"%d\" as=\"geometry\" />",
-                    xPosition, dynamicWidth, totalHeight));
-            xml.append("</mxCell>");
-
+            // Add Rows
             for (int f = 0; f < table.fields.size(); f++) {
-                Field field = table.fields.get(f);
-                String label = String.format("+ %s (%s): %s", field.cyrillicName, field.englishName, field.type);
-
-                // Row Style
-                String rowStyle = "shape=tableRow;horizontal=1;startSize=0;swimlaneHead=0;swimlaneBody=0;top=0;left=0;bottom=0;right=0;" +
-                        "collapsible=0;dropTarget=0;fillColor=none;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" +
-                        "align=left;verticalAlign=middle;spacingLeft=10;whiteSpace=wrap;html=1;fontSize=11;";
-
-                int yOffset = headerHeight + (f * rowHeight);
-                xml.append(String.format("<mxCell id=\"%s_row_%d\" value=\"%s\" style=\"%s\" vertex=\"1\" parent=\"%s\">",
-                        tableId, f, label, rowStyle, tableId));
-                xml.append(String.format("<mxGeometry y=\"%d\" width=\"%d\" height=\"%d\" as=\"geometry\" />",
-                        yOffset, dynamicWidth, rowHeight));
-                xml.append("</mxCell>");
+                addRow(xml, table.fields.get(f), tableId, f, dynamicWidth);
             }
 
-            xPosition += (dynamicWidth + 50); // Offset next table by its own width
+            xPosition += (dynamicWidth + SPACING);
         }
 
         xml.append("</root></mxGraphModel></diagram></mxfile>");
         saveToFile(xml.toString(), "dynamic_schema.drawio");
+    }
+
+    private void addTable(StringBuilder xml, Table table, String id, int x, int width, int height) {
+        String style = "shape=table;startSize=" + HEADER_HEIGHT + ";container=1;collapsible=0;childLayout=0;" +
+                "fixedRows=1;rowLines=1;fontStyle=1;align=center;rounded=1;arcSize=12;whiteSpace=wrap;html=1;";
+
+        xml.append(String.format("<mxCell id=\"%s\" value=\"%s\" style=\"%s\" vertex=\"1\" parent=\"1\">",
+                id, table.name, style));
+        xml.append(String.format("<mxGeometry x=\"%d\" y=\"80\" width=\"%d\" height=\"%d\" as=\"geometry\" />",
+                x, width, height));
+        xml.append("</mxCell>");
+    }
+
+    private void addRow(StringBuilder xml, Field field, String tableId, int index, int width) {
+        String label = String.format("+ %s (%s): %s", field.cyrillicName, field.englishName, field.type);
+        String style = "shape=tableRow;horizontal=1;startSize=0;swimlaneHead=0;swimlaneBody=0;top=0;left=0;bottom=0;right=0;" +
+                "collapsible=0;dropTarget=0;fillColor=none;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" +
+                "align=left;verticalAlign=middle;spacingLeft=10;whiteSpace=wrap;html=1;fontSize=11;";
+
+        int yOffset = HEADER_HEIGHT + (index * ROW_HEIGHT);
+
+        xml.append(String.format("<mxCell id=\"%s_row_%d\" value=\"%s\" style=\"%s\" vertex=\"1\" parent=\"%s\">",
+                tableId, index, label, style, tableId));
+        xml.append(String.format("<mxGeometry y=\"%d\" width=\"%d\" height=\"%d\" as=\"geometry\" />",
+                yOffset, width, ROW_HEIGHT));
+        xml.append("</mxCell>");
+    }
+
+    private int calculateWidth(Table table) {
+        int maxChars = table.name.length();
+        for (Field f : table.fields) {
+            String fullString = "+ " + f.cyrillicName + " (" + f.englishName + "): " + f.type;
+            maxChars = Math.max(maxChars, fullString.length());
+        }
+        // Heuristic: ~6 pixels per char + padding.
+        return Math.max(180, (maxChars * 6) + 40);
     }
 
     private void saveToFile(String content, String filename) {
